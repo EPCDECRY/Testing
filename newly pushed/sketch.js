e0b292015b042ca7c90141165d16caca88727026
atinvento100 <65214776+atinvento100@users.noreply.github.com>
2020-06-29 11:57:23 +0530

    newly pushed floder
    please see under newly pushed floder and combine...

---
+++ newly pushed/sketch.js
@@ -0,0 +1,23 @@
+
+
+function func3(){
+  var x = document.getElementById("th");
+  var y = document.getElementById("fo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function func4(){
+  var x = document.getElementById("fo");
+  var y = document.getElementById("th");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
