cdcbb22477f8dc640611bee0d4f50d21790994d3
SSRaj001 <rajendraprasath01@hotmail.com>
2020-06-30 13:10:56 +0530

    Changes to Some files

--- sketch.js
+++ sketch.js
@@ -1,9 +1,7 @@
-
-
 function func3(){
-  var tt3 = document.getElementById("th");
-  var opt = document.getElementById("select-option");
-  var tt4 = document.getElementById("fo");
+  let tt3 = document.getElementById("th");
+  let opt = document.getElementById("select-option");
+  let tt4 = document.getElementById("fo");
   if (tt3.style.display === "none") {
     opt.style.display = "none";
     tt4.style.display = "none";
@@ -16,16 +14,34 @@ function func3(){
 }
 //
 function func4(){
-var tt4 = document.getElementById("fo");
-var tt3 = document.getElementById("th");
-var opt = document.getElementById("select-option");
-if (tt4.style.display === "none") {
-  tt4.style.display = "block";
-   tt3.style.display = "none";
-   opt.style.display = "none";
-  } else {
-    tt4.style.display = "none";
+  let tt4 = document.getElementById("fo");
+  let tt3 = document.getElementById("th");
+  let opt = document.getElementById("select-option");
+  if (tt4.style.display === "none") {
+    tt4.style.display = "block";
     tt3.style.display = "none";
-    opt.style.display = "block";
-  }
+    opt.style.display = "none";
+    } else {
+      tt4.style.display = "none";
+      tt3.style.display = "none";
+      opt.style.display = "block";
+    }
+}
+
+function funcpvp(){
+  let p1 = document.getElementById('human');
+  let p2 = document.getElementById('robot');
+  p1.src = "img/Human1.png";
+  p2.src = "img/Human2.png";
+  document.getElementById('pvp').style.display = "none";
+  document.getElementById('pve').style.display = "block";
+}
+
+function funcpve(){
+  let p1 = document.getElementById('human');
+  let p2 = document.getElementById('robot');
+  p1.src = "img/Human1.png";
+  p2.src = "img/Robot.png";
+  document.getElementById('pvp').style.display = "block";
+  document.getElementById('pve').style.display = "none";
 }
