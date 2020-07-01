508408bfa695be5c98322da38aff49dd8213a09c
atinvento100 <65214776+atinvento100@users.noreply.github.com>
2020-07-01 16:59:09 +0530

    including minimax algorithm

--- Testing2/src/aiminimax.js
+++ Testing2/src/aiminimax.js
@@ -1,4 +1,57 @@
 
 function bestSPOT(){
-  return emptySquares()[0];
+  return MiniMax(origBoard, ai).index;
+}
+
+
+
+function MiniMax(board, player) {
+	var availSpots = emptySquares();
+
+	if (checkEndgame(board, human)) {
+		return {score: -10};
+	} else if (checkEndgame(board, ai)) {
+		return {score: 10};
+	} else if (availSpots.length === 0) {
+		return {score: 0};
+	}
+	var moves = [];
+	for (var i = 0; i < availSpots.length; i++) {
+		var move = {};
+		move.index = board[availSpots[i]];
+		board[availSpots[i]] = player;
+
+		if (player == ai) {
+			var result = MiniMax(board, human);
+			move.score = result.score;
+		} else {
+			var result = MiniMax(board, ai);
+			move.score = result.score;
+		}
+
+		board[availSpots[i]] = move.index;
+
+		moves.push(move);
+	}
+
+	var bestMove;
+	if(player === ai) {
+		var bestScore = -Infinity;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score > bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	} else {
+		var bestScore = Infinity;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+
+	return moves[bestMove];
 }
